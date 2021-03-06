2c52ad06a17ce071344b50cbe1310c03ac82d568
張誌叡 <ray_chang@mayohr.com>
2021-03-06 16:30:49 +0800

    更新樣式

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -29,6 +29,9 @@ function Calculator() {
       case 'x':
         setSum(Number(firstNum) * Number(secondNum));
         break;
+      case '÷':
+        setSum(Number(firstNum) / Number(secondNum));
+        break;
       case '-':
         setSum(Number(firstNum) - Number(secondNum));
         break;
@@ -45,25 +48,30 @@ function Calculator() {
     <div className="wrapper">
       <input className="display" readOnly value={firstNum ? `${firstNum}${action}${secondNum}` : sum}/>
       <div className="container">
+        <div className="btn tool" >AC</div>
+        <div className="btn tool" >±</div>
+        <div className="btn tool" >%</div>
+        <div className="btn action" onClick={handleActionClick} data-action="÷">÷</div>
+
         <div className="btn" onClick={handleNumClick} data-number="7">7</div>
         <div className="btn" onClick={handleNumClick} data-number="8">8</div>
         <div className="btn" onClick={handleNumClick} data-number="9">9</div>
-        <div className="btn" onClick={handleActionClick} data-action="x">x</div>
+        <div className="btn action" onClick={handleActionClick} data-action="x">x</div>
 
         <div className="btn" onClick={handleNumClick} data-number="4">4</div>
         <div className="btn" onClick={handleNumClick} data-number="5">5</div>
         <div className="btn" onClick={handleNumClick} data-number="6">6</div>
-        <div className="btn" onClick={handleActionClick} data-action="-" >-</div>
+        <div className="btn action" onClick={handleActionClick} data-action="-" >-</div>
 
         <div className="btn" onClick={handleNumClick} data-number="1">1</div>
         <div className="btn" onClick={handleNumClick} data-number="2">2</div>
         <div className="btn" onClick={handleNumClick} data-number="3">3</div>
-        <div className="btn" onClick={handleActionClick} data-action="+">+</div>
+        <div className="btn action" onClick={handleActionClick} data-action="+">+</div>
 
-        <div className="btn" >±</div>
         <div className="btn" onClick={handleNumClick} data-number="0">0</div>
         <div className="btn" onClick={handleNumClick} data-number="00">00</div>
-        <div className="btn" onClick={handleCalculation}>＝</div>
+        <div className="btn" >.</div>
+        <div className="btn action" onClick={handleCalculation}>＝</div>
       </div>
     </div>
   );
